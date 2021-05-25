3fbaeea3196d3ae9579781b3121f868cd8313314
Diego Avellaneda <avellanedad@javeriana.edu.co>
2021-05-25 11:50:48 -0500

    update scanning script

--- BLE_Scanner/BLE_Scanner.ino
+++ BLE_Scanner/BLE_Scanner.ino
@@ -11,11 +11,23 @@
 int scanTime = 5; //In seconds
 BLEScan* pBLEScan;
 
+
+String Names[10];
+int Device_RSSI[10];
+// index del arreglo spara nombres y RSSI 
+int scan_position=0;
+
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-        Serial.print("  RSSI:");
-        Serial.println(advertisedDevice.getRSSI());
+      //Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+        //Serial.print("  RSSI:");
+        //Serial.println(advertisedDevice.getRSSI());
+        if(advertisedDevice.haveName()== true){
+                  Names[scan_position]= advertisedDevice.getName().c_str();
+                  Device_RSSI[scan_position]=advertisedDevice.getRSSI();
+                  scan_position ++; 
+          }
+
     }
 };
 
@@ -30,16 +42,62 @@ void setup() {
   pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
   pBLEScan->setInterval(100);
   pBLEScan->setWindow(99);  // less or equal setInterval value
+
+      // Limpiar los dos Array 
+  for (int i=0; i< (sizeof(Device_RSSI)/sizeof(Device_RSSI[0]));i++){
+    Device_RSSI[i]=0;
+  }
+   for (int i=0; i< (sizeof(Names)/sizeof(Names[0]));i++){
+    Names[i]="";
+  }
 }
 
 void loop() {
   BLEDevice::init("");
   // put your main code here, to run repeatedly:
   BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
-  Serial.print("Devices found: ");
-  Serial.println(foundDevices.getCount());
-  Serial.println("Scan done!");
+  //Serial.print("Devices found: ");
+  //Serial.println(foundDevices.getCount());
+  //Serial.println("Scan done!");
+  PrintJSON();
   pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  
+  scan_position=0;
+
+  // Limpiar los dos Array 
+  for (int i=0; i< (sizeof(Device_RSSI)/sizeof(Device_RSSI[0]));i++){
+     Device_RSSI[i]=0;
+  }
+  for (int i=0; i< (sizeof(Names)/sizeof(Names[0]));i++){
+      Names[i]="";
+  }
+    
   delay(700);
   BLEDevice::deinit(false);
+}
+
+void PrintJSON(){
+  Serial.print("{\"devices\":[");
+  for (int i=0; i< (sizeof(Names)/sizeof(Names[0]));i++){
+    if(Names[i]==""){
+      Serial.println("]}");
+      break;
+    }
+    else{
+      Serial.print("{\"name\":");
+      Serial.print("\"");
+      Serial.print(Names[i]);
+      Serial.print("\",");
+      Serial.print("\"rssi\":");
+      Serial.print(Device_RSSI[i]);
+    
+      if(Names[i+1]==""){
+        Serial.print("}");
+      }
+      else{
+        Serial.print("},");
+      }
+    }
+  }
+  
 }

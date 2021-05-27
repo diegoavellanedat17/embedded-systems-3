e196145e4a499959a389a92f87307254eb0dc135
Diego Avellaneda <avellanedad@javeriana.edu.co>
2021-05-26 19:27:55 -0500

    multiproceso con serial

--- scanner.py
+++ scanner.py
@@ -1,8 +1,21 @@
 
 from time import sleep
 import serial
+import time
+import threading
+import queue
+#Tener un hilo para que cada que reciba un dato serial imprima en pantalla
 
 ser = serial.Serial ("/dev/ttyS0", 115200)    #Open port with baud rate
+
+qBLE_Data=queue.Queue()
+
+def serial_worker():
+    item=qBLE_Data.get()
+    print(item)
+    # Aqui se hará append al archivo
+
+
 while True:
     received_data = ser.read()              #read serial port
     sleep(0.03)
@@ -10,7 +23,7 @@ while True:
     received_data += ser.read(data_left)
     try:    	
     	received_data=received_data.decode('utf8')
-    	print(received_data)
+        qBLE_Data.put(received_data)
     except Exception as e:
     	print(e)
 
